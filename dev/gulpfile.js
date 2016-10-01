419e97d8c8e334fd60a8096e4c5d396297a47cf7
loveKino <842914439@qq.com>
2016-10-01 23:48:29 +0800

    init

---
+++ dev/gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let gulp = require('gulp');
+
+let argv = require('yargs').argv;
+
+let {
+    install
+} = require('nodedevdeps');
+
+let path = require('path');
+
+let spawnp = require('spawnp');
+
+let del = require('del');
+
+let promisify = require('promisify-node');
+
+let ncp = promisify(require('ncp'));
+
+const publishDir = path.join(__dirname, './publish');
+
+gulp.task('prepublish', () => {
+    let project = depMap[argv.name];
+    let publicPath = path.join(publishDir, argv.name);
+    return del([publicPath]).then(() => {
+        return ncp(project.path, publicPath);
+    }).then(() => {
+        return install(publicPath).then(() => {
+            return spawnp('npm test', null, {
+                cwd: publicPath,
+                stdio: 'inherit'
+            });
+        });
+    });
+});
+
+gulp.task('publish', ['prepublish'], () => {
+    return spawnp('npm publish', null, {
+        cwd: path.join(publishDir, argv.name),
+        stdio: 'inherit'
+    });
+});
